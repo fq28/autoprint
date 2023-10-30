1a553c35284a05b753ceb11929b32ca9504de8b1
Florian de Jager <33357810+fq28@users.noreply.github.com>
2023-10-30 19:35:28 +0100

    Changed to Sumatra for faster printing, added the possibility of printing different files to different printers

--- autoprint.py
+++ autoprint.py
@@ -1,36 +1,39 @@
 import os
 import time
 import win32print
-import win32api
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from collections import deque
+import threading
+from subprocess import call
 
-# Folder to monitor (can be adjusted as per need)
+# Folder to monitor
 WATCHED_FOLDER = 'E:\\Picture'
-PRINT_QUEUE = deque()
-PRINTING = False
+
+# program that will be used for printing
+SUMATRA_DIR = "C:\\Users\\fqdej\\AppData\\Local\\SumatraPDF\\SumatraPDF.exe" 
+
+# Mapping of filename prefixes to printer names
+PRINTER_MAPPING = {
+    'PACKINGSLIP': 'ET-7700 Series(Network)',  # Replace with actual printer name
+    'DEFAULT': 'ZDesigner GK420d'  # Default printer for labels
+}
 
 class OnMyWatch:
-    # Set the directory
     WATCHED_FOLDER = WATCHED_FOLDER
 
-    # Define event handler
     class Handler(FileSystemEventHandler):
         @staticmethod
-        def on_moved(event):
+        def on_created(event):
             file = event.src_path.removesuffix(".crdownload")
 
-            # Windows1 10:if '.pdf' in event.src_path and os.path.exists(event.src_path):  # Check if a pdf file and if it still exists
-            if '.pdf' in event.src_path:
-                print(f"Received file: {file}. Adding to print queue...")
-                PRINT_QUEUE.append(file)
-                process_queue()
-
+            # check if the file is a pdf
+            if '.pdf' in file and os.path.exists(file):
+                print(f"Received file: {file}. Starting print thread...")
+                threading.Thread(target=process_file, args=(file,)).start()
 
     def __init__(self):
         self.observer = Observer()
-
+    
     def run(self):
         event_handler = OnMyWatch.Handler()
         self.observer.schedule(event_handler, self.WATCHED_FOLDER, recursive=False)
@@ -43,44 +46,44 @@ class OnMyWatch:
             print("Observer stopped.")
         self.observer.join()
 
-def process_queue():
-    global PRINTING, PRINT_QUEUE
-    if PRINTING:
-        return  # Return if a print job is already in progress
+def process_file(file_path):
+    try:
+        time.sleep(1)  # Wait for file to be completely written
 
-    if PRINT_QUEUE:
-        PRINTING = True
-        file_path = PRINT_QUEUE.popleft()  # Fetch the next file from the queue
+        filename = os.path.basename(file_path)
+        printer_name = PRINTER_MAPPING['DEFAULT']
+        for prefix, printer in PRINTER_MAPPING.items():
+            if filename.startswith(prefix):
+                printer_name = printer
+                break
         
-        print(f"Waiting for file {file_path} to settle...")
-        time.sleep(1)
-        
-        print(f"Now printing {file_path}...")
-        print_file(file_path)
-        
-        print(f"Finished printing {file_path}. Deleting now...")
-        time.sleep(5)
+        print(f"Now printing {file_path} on printer {printer_name}...")
+        print_file(file_path, printer_name)
+
+        time.sleep(5)  # Give some time to finish printing
         os.remove(file_path)
-        
-        PRINTING = False
-        process_queue()  # Check the queue again
+        print(f"Deleted {file_path}. ")
+
+    except Exception as e:
+        print("Eror while printing:", e)
 
 
-def print_file(file_path):
+def printer_does_exists(printer_name):
+    # Get the list of all installed printers
+    printers = [printer[2] for printer in win32print.EnumPrinters(win32print.PRINTER_ENUM_LOCAL | win32print.PRINTER_ENUM_CONNECTIONS)]
+
+    # Check if the specified printer is in the list of installed printers
+    if printer_name not in printers:
+        print(f"Error: Printer '{printer_name}' not found. Available printers are: {printers}")
+        return False
+    return True
+
+def print_file(file_path, printer_name):
     """
-    Print the file using default printer.
+    Print the file using the specified printer.
     """
-    printer_name = win32print.GetDefaultPrinter()
-    win32api.ShellExecute(
-        0,
-        "print",
-        file_path,
-        '/d:"%s"' % printer_name,
-        ".",
-        0
-    )
-    process_queue()
-
+    if printer_does_exists(printer_name):
+        call([SUMATRA_DIR, "-print-to", printer_name, "-silent", file_path])
 
 if __name__ == '__main__':
     watch = OnMyWatch()
